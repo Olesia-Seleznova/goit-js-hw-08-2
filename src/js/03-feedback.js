c9ac38f6832b4ffee3bd10ef4396d6fbf6bbeb86
Olesia-Seleznova <106627666+Olesia-Seleznova@users.noreply.github.com>
2023-07-03 22:50:30 +0300

    feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('form'),
+  input: document.querySelector('input'),
+  textarea: document.querySelector('textarea'),
+}
+
+populateFormData();
+
+refs.form.addEventListener("submit", throttle(onInputSubmit, 500));
+
+refs.form.addEventListener("input", event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+})
+
+function onInputSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
+}
+
+function populateFormData() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  const parsedFormData = JSON.parse(savedFormData);
+  if (parsedFormData) {
+    refs.input.value = parsedFormData.email || '';
+    refs.textarea.value = parsedFormData.message || '';
+    console.log(parsedFormData);
+  }
+}
